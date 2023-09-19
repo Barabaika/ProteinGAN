53157b55f49b5ea2d6e54a208728e4f2c39bcce4
Shevtsov <shevtsov@airi.net>
2023-09-20 00:12:31 +0300

    non standart ams change

--- src/gan/protein/helpers.py
+++ src/gan/protein/helpers.py
@@ -10,7 +10,7 @@ REAL_PROTEINS = "real_proteins"
 FAKE_PROTEINS = "fake_proteins"
 CLASS_MAPPING = "class_mapping"
 LABELS = "labels"
-NUM_AMINO_ACIDS = 21
+NUM_AMINO_ACIDS = 27
 SEQ_LENGTH = "seq_length"
 
 
